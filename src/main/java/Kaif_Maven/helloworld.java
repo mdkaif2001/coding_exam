17eec6cd17d52e912e3d42f6aabb691c58c9b5bd
mdkaif2001 <112816935+mdkaif2001@users.noreply.github.com>
2022-09-19 15:27:28 +0530

    Add files via upload

---
+++ src/main/java/Kaif_Maven/helloworld.java
@@ -0,0 +1,8 @@
+package Kaif_Maven;
+
+public class helloworld {
+	public void HelloWorld() {
+		System.out.println("Hello, Welcome to Docker");
+	}
+
+}
---
+++ src/test/java/Kaif_Maven/JUnit.java
@@ -0,0 +1,14 @@
+package Kaif_Maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnit {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
